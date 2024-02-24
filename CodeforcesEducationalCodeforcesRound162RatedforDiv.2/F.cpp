61eb5e3b1c38fa94505d5bfff3dfc98fb8815534
Tobias Andersen <tobiasa.jobs@gmail.com>
2024-02-24 03:08:20 +0100

    .

---
+++ CodeforcesEducationalCodeforcesRound162RatedforDiv.2/F.cpp
@@ -0,0 +1,349 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+
+template<class T>
+constexpr T power(T a, ll b) {
+    T res = 1;
+    for (; b; b /= 2, a *= a) {
+        if (b % 2) {
+            res *= a;
+        }
+    }
+    return res;
+}
+
+constexpr ll mul(ll a, ll b, ll p) {
+    ll res = a * b - ll(1.L * a * b / p) * p;
+    res %= p;
+    if (res < 0) {
+        res += p;
+    }
+    return res;
+}
+template<ll P>
+struct MLong {
+    ll x;
+    constexpr MLong() : x{} {}
+    constexpr MLong(ll x) : x{norm(x % getMod())} {}
+    
+    static ll Mod;
+    constexpr static ll getMod() {
+        if (P > 0) {
+            return P;
+        } else {
+            return Mod;
+        }
+    }
+    constexpr static void setMod(ll Mod_) {
+        Mod = Mod_;
+    }
+    constexpr ll norm(ll x) const {
+        if (x < 0) {
+            x += getMod();
+        }
+        if (x >= getMod()) {
+            x -= getMod();
+        }
+        return x;
+    }
+    constexpr ll val() const {
+        return x;
+    }
+    explicit constexpr operator ll() const {
+        return x;
+    }
+    constexpr MLong operator-() const {
+        MLong res;
+        res.x = norm(getMod() - x);
+        return res;
+    }
+    constexpr MLong inv() const {
+        assert(x != 0);
+        return power(*this, getMod() - 2);
+    }
+    constexpr MLong &operator*=(MLong rhs) & {
+        x = mul(x, rhs.x, getMod());
+        return *this;
+    }
+    constexpr MLong &operator+=(MLong rhs) & {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    constexpr MLong &operator-=(MLong rhs) & {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    constexpr MLong &operator/=(MLong rhs) & {
+        return *this *= rhs.inv();
+    }
+    friend constexpr MLong operator*(MLong lhs, MLong rhs) {
+        MLong res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend constexpr MLong operator+(MLong lhs, MLong rhs) {
+        MLong res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend constexpr MLong operator-(MLong lhs, MLong rhs) {
+        MLong res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend constexpr MLong operator/(MLong lhs, MLong rhs) {
+        MLong res = lhs;
+        res /= rhs;
+        return res;
+    }
+    friend constexpr std::istream &operator>>(std::istream &is, MLong &a) {
+        ll v;
+        is >> v;
+        a = MLong(v);
+        return is;
+    }
+    friend constexpr std::ostream &operator<<(std::ostream &os, const MLong &a) {
+        return os << a.val();
+    }
+    friend constexpr bool operator==(MLong lhs, MLong rhs) {
+        return lhs.val() == rhs.val();
+    }
+    friend constexpr bool operator!=(MLong lhs, MLong rhs) {
+        return lhs.val() != rhs.val();
+    }
+};
+
+template<>
+ll MLong<0LL>::Mod = ll(1E18) + 9;
+
+template<int P>
+struct MInt {
+    int x;
+    constexpr MInt() : x{} {}
+    constexpr MInt(ll x) : x{norm(x % getMod())} {}
+    
+    static int Mod;
+    constexpr static int getMod() {
+        if (P > 0) {
+            return P;
+        } else {
+            return Mod;
+        }
+    }
+    constexpr static void setMod(int Mod_) {
+        Mod = Mod_;
+    }
+    constexpr int norm(int x) const {
+        if (x < 0) {
+            x += getMod();
+        }
+        if (x >= getMod()) {
+            x -= getMod();
+        }
+        return x;
+    }
+    constexpr int val() const {
+        return x;
+    }
+    explicit constexpr operator int() const {
+        return x;
+    }
+    constexpr MInt operator-() const {
+        MInt res;
+        res.x = norm(getMod() - x);
+        return res;
+    }
+    constexpr MInt inv() const {
+        assert(x != 0);
+        return power(*this, getMod() - 2);
+    }
+    constexpr MInt &operator*=(MInt rhs) & {
+        x = 1LL * x * rhs.x % getMod();
+        return *this;
+    }
+    constexpr MInt &operator+=(MInt rhs) & {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator-=(MInt rhs) & {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator/=(MInt rhs) & {
+        return *this *= rhs.inv();
+    }
+    friend constexpr MInt operator*(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend constexpr MInt operator+(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend constexpr MInt operator-(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend constexpr MInt operator/(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res /= rhs;
+        return res;
+    }
+    friend constexpr std::istream &operator>>(std::istream &is, MInt &a) {
+        ll v;
+        is >> v;
+        a = MInt(v);
+        return is;
+    }
+    friend constexpr std::ostream &operator<<(std::ostream &os, const MInt &a) {
+        return os << a.val();
+    }
+    friend constexpr bool operator==(MInt lhs, MInt rhs) {
+        return lhs.val() == rhs.val();
+    }
+    friend constexpr bool operator!=(MInt lhs, MInt rhs) {
+        return lhs.val() != rhs.val();
+    }
+};
+
+template<>
+int MInt<0>::Mod = 998244353;
+
+template<int V, int P>
+constexpr MInt<P> CInv = MInt<P>(V).inv();
+
+constexpr int P = 1000000007;
+using Z = MInt<P>;
+struct SuffixArray {
+    int n;
+    std::vector<int> sa, rk, lc;
+    SuffixArray(const std::string &s) {
+        n = s.length();
+        sa.resize(n);
+        lc.resize(n - 1);
+        rk.resize(n);
+        std::iota(sa.begin(), sa.end(), 0);
+        std::sort(sa.begin(), sa.end(), [&](int a, int b) {return s[a] < s[b];});
+        rk[sa[0]] = 0;
+        for (int i = 1; i < n; ++i)
+            rk[sa[i]] = rk[sa[i - 1]] + (s[sa[i]] != s[sa[i - 1]]);
+        int k = 1;
+        std::vector<int> tmp, cnt(n);
+        tmp.reserve(n);
+        while (rk[sa[n - 1]] < n - 1) {
+            tmp.clear();
+            for (int i = 0; i < k; ++i)
+                tmp.push_back(n - k + i);
+            for (auto i : sa)
+                if (i >= k)
+                    tmp.push_back(i - k);
+            std::fill(cnt.begin(), cnt.end(), 0);
+            for (int i = 0; i < n; ++i)
+                ++cnt[rk[i]];
+            for (int i = 1; i < n; ++i)
+                cnt[i] += cnt[i - 1];
+            for (int i = n - 1; i >= 0; --i)
+                sa[--cnt[rk[tmp[i]]]] = tmp[i];
+            std::swap(rk, tmp);
+            rk[sa[0]] = 0;
+            for (int i = 1; i < n; ++i)
+                rk[sa[i]] = rk[sa[i - 1]] + (tmp[sa[i - 1]] < tmp[sa[i]] || sa[i - 1] + k == n || tmp[sa[i - 1] + k] < tmp[sa[i] + k]);
+            k *= 2;
+        }
+        for (int i = 0, j = 0; i < n; ++i) {
+            if (rk[i] == 0) {
+                j = 0;
+            } else {
+                for (j -= j > 0; i + j < n && sa[rk[i] - 1] + j < n && s[i + j] == s[sa[rk[i] - 1] + j]; )
+                    ++j;
+                lc[rk[i] - 1] = j;
+            }
+        }
+    }
+};
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int n, k;
+    std::cin >> n >> k;
+    
+    std::string s;
+    std::cin >> s;
+    
+    std::string t = s;
+    
+    int res = k;
+    int r = n;
+    for (int i = 0; i < n; i++) {
+        if (t[i] == '1') {
+            while (r > 0 && t[r - 1] == '1') {
+                r--;
+            }
+            if (res > 0 && i < r) {
+                std::swap(t[i], t[r - 1]);
+                res--;
+            }
+        }
+    }
+    
+    auto work = [&](int k) {
+        int cnt = std::count(s.begin(), s.end(), '1');
+        int need = std::max(1, cnt - k);
+        std::vector<int> p;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '1') {
+                p.push_back(i);
+            }
+        }
+        int len = n;
+        for (int i = need - 1; i < p.size(); i++) {
+            len = std::min(len, p[i] - p[i - (need - 1)] + 1);
+        }
+        if (len <= cnt) {
+            t = std::string(n - cnt, '0') + std::string(cnt, '1');
+            return;
+        }
+        std::vector<int> pre(n + 1);
+        SuffixArray sa(s);
+        for (int i = 0; i < n; i++) {
+            pre[i + 1] = pre[i] + s[i] - '0';
+        }
+        for (auto i : sa.sa) {
+            if (i + len <= n && pre[i + len] - pre[i] >= need) {
+                std::string ans = s.substr(i, len);
+                int res = k;
+                for (int i = ans.size() - 1; i >= 0; i--) {
+                    if (ans[i] == '0' && res > 0) {
+                        res--;
+                        ans[i] = '1';
+                    }
+                }
+                ans = std::string(n - len, '0') + ans;
+                t = std::min(t, ans);
+                return;
+            }
+        }
+        assert(false);
+    };
+    
+    if (k >= 2) {
+        work(k - 2);
+    }
+    if (k >= 1) {
+        std::reverse(s.begin(), s.end());
+        work(k - 1);
+    }
+    Z ans = 0;
+    for (auto c : t) {
+        ans = ans * 2 + (c - '0');
+    }
+    std::cout << ans << "\n";
+    
+    return 0;
+}
